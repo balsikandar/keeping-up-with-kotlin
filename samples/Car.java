779e5f10df0f7f7da97d8695b9d6e8e01928f9b7
balsikandar <bal.sikandar@yahoo.com>
2018-06-15 11:31:21 +0530

    update folder

--- samples/Car.java
+++ samples/Car.java
@@ -1,11 +1,17 @@
 package com.balsikandar.model;
 
+/**
+ * Created by bali on 15/06/18.
+ */
+
+
+
 public class Car {
 
-  private int yearModel;
-  private String make;
-  private boolean isAutomatic;
-  
+    private int yearModel;
+    private String make;
+    private boolean isAutomatic;
+
     public Car(int yearModel, String make) {
         this.yearModel = yearModel;
         this.make = make;
